e0eae74c91aed7f1634b7735a8ab0c50c5b2425e
Cathy Huang <cathyhaung.tw@gmail.com>
2017-12-16 02:54:25 -0800

    add github link

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 //import { Form, FormGroup, Input, Button } from 'reactstrap';
+import github from '../images/github.png';
 import '../styles/Footer.css';
 
 export default class Footer extends Component {
@@ -9,6 +10,8 @@ export default class Footer extends Component {
       <footer className="footer-bottom">
         <div className="container">
           <span className="text-muted">© Copyright 2017 - Lego Exchanger. All rights reserved.</span>
+          <a href="https://github.com/rastaturin/LegoXWeb" target="_blank"><img src={github} alt="github" className="githubIcon" />
+          </a>
         </div>
       </footer>
     )
